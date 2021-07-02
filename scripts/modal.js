532624f00b114c23ce88b3ad17f714d0c5a7bc7d
Lucas Nunes <55895056+luk3mn@users.noreply.github.com>
2021-07-02 19:00:14 -0300

    Subindo o front-end do projeto

---
+++ scripts/modal.js
@@ -0,0 +1,18 @@
+/******  ******/
+document.querySelector('#open').addEventListener("click", () => {
+  document.querySelector('.box-modal').style.width='300px';
+  document.querySelector('.box-modal').style.transition='.3s';
+})
+
+document.querySelector('#close').addEventListener("click", () => {
+  document.querySelector('.box-modal').style.width='0px';
+})
+
+/***** BUY *****/
+document.querySelector('#open-buy').addEventListener("click", () => {
+  document.querySelector('.buy-modal').classList.remove('hidden');
+})
+
+document.querySelector('#close-buy').addEventListener("click", () => {
+  document.querySelector('.buy-modal').classList.add('hidden');
+})
